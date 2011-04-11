b83d37baeadc3e5e650c2c16d15e5fb19f7e32c5
Jon Ohrt <jonohrt1@gmail.com>
2011-04-11 10:49:45 -0700

    updated spec helper to remove include expectations line

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,7 @@ RSpec.configure do |config|
   # Remove this line if you don't want RSpec's should and should_not
   # methods or matchers
   require 'rspec/expectations'
-  config.include RSpec::Matchers
+  #config.include RSpec::Matchers
 
   # == Mock Framework
   config.mock_with :rspec
