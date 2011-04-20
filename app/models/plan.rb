b2654173027b2244503c2df8edbfc48ac22d60a3
Jon Ohrt <jonohrt1@gmail.com>
2011-04-20 16:40:08 -0700

    update respond_to for plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -6,9 +6,23 @@ class Plan < ActiveRecord::Base
   validates_presence_of :price, :message => "can't be blank"
   
   has_many :subscriptions
-  
-  BILLING_PERIODS = ["Monthly", "Annually"]
 
+  BILLING_PERIODS = ["Monthly", "Annually"]
+  
+  def respond_to?(method_sym, include_private = false)
+      @column_names = []
+      unless dynamic_attributes.nil?
+        dynamic_attributes.each do |dm|
+           @column_names << dm[0].gsub("field_", "")
+         end
+        if @column_names.include? method_sym.to_s
+          return  true
+        end
+      end
+
+    super
+  end
+    
   def method_missing(method, *args)
     
     if has_dynamic_attribute?("field_#{method}")
